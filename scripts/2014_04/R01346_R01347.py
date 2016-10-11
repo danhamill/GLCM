fa5409bd633c75f72e575f63a9fe83cac01c5418
danhamill <hamill.daniel@gmail.com>
2016-10-11 13:41:14 -0700

    inital commit

---
+++ scripts/2014_04/R01346_R01347.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 10:56:18 2016
+
+@author: dan
+"""
+
+import gdal, osr
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+from numpy.lib.stride_tricks import as_strided as ast
+import dask.array as da
+from joblib import Parallel, delayed
+import os
+
+def im_resize(im,Nx,Ny):
+    '''
+    resize array by bivariate spline interpolation
+    '''
+    ny, nx = np.shape(im)
+    xx = np.linspace(0,nx,Nx)
+    yy = np.linspace(0,ny,Ny)
+    
+    try:
+        im = da.from_array(im, chunks=1000)   #dask implementation
+    except:
+        pass
+
+    newKernel = RectBivariateSpline(np.r_[:ny],np.r_[:nx],im)
+    return newKernel(yy,xx)
+    
+def p_me(Z):
+    '''
+    loop to standard deviation
+    '''
+    try:
+        std = np.std(Z)
+        return (std)
+    except:
+        print 'Something went wrong'
+        return (np.nan)
+        
+        
+def read_raster(in_raster):
+    in_raster=in_raster
+    ds = gdal.Open(in_raster)
+    data = ds.GetRasterBand(1).ReadAsArray()
+    data[data<=0] = np.nan
+    gt = ds.GetGeoTransform()
+    xres = gt[1]
+    yres = gt[5]
+    
+    # get the edge coordinates and add half the resolution 
+    # to go to center coordinates
+    xmin = gt[0] + xres * 0.5
+    xmax = gt[0] + (xres * ds.RasterXSize) - xres * 0.5
+    ymin = gt[3] + (yres * ds.RasterYSize) + yres * 0.5
+    ymax = gt[3] - yres * 0.5
+    del ds
+    # create a grid of xy coordinates in the original projection
+    xx, yy = np.mgrid[xmin:xmax+xres:xres, ymax+yres:ymin:yres]
+    return data, xx, yy, gt
+    
+def norm_shape(shap):
+   '''
+   Normalize numpy array shapes so they're always expressed as a tuple,
+   even for one-dimensional shapes.
+   '''
+   try:
+      i = int(shap)
+      return (i,)
+   except TypeError:
+      # shape was not a number
+      pass
+
+   try:
+      t = tuple(shap)
+      return t
+   except TypeError:
+      # shape was not iterable
+      pass
+
+   raise TypeError('shape must be an int, or a tuple of ints')
+
+def sliding_window(a, ws, ss = None, flatten = True):
+    '''
+    Return a sliding window over a in any number of dimensions
+    '''      
+    if None is ss:
+        # ss was not provided. the windows will not overlap in any direction.
+        ss = ws
+    ws = norm_shape(ws)
+    ss = norm_shape(ss)
+    # convert ws, ss, and a.shape to numpy arrays
+    ws = np.array(ws)
+    ss = np.array(ss)
+    shap = np.array(a.shape)
+    # ensure that ws, ss, and a.shape all have the same number of dimensions
+    ls = [len(shap),len(ws),len(ss)]
+    if 1 != len(set(ls)):
+        raise ValueError(\
+        'a.shape, ws and ss must all have the same length. They were %s' % str(ls))
+    
+    # ensure that ws is smaller than a in every dimension
+    if np.any(ws > shap):
+        raise ValueError(\
+        'ws cannot be larger than a in any dimension.\
+     a.shape was %s and ws was %s' % (str(a.shape),str(ws)))
+     
+    # how many slices will there be in each dimension?
+    newshape = norm_shape(((shap - ws) // ss) + 1)
+    
+    
+    # the shape of the strided array will be the number of slices in each dimension
+    # plus the shape of the window (tuple addition)
+    newshape += norm_shape(ws)
+    
+    
+    # the strides tuple will be the array's strides multiplied by step size, plus
+    # the array's strides (tuple addition)
+    newstrides = norm_shape(np.array(a.strides) * ss) + a.strides
+    a = ast(a,shape = newshape,strides = newstrides)
+    if not flatten:
+        return a
+    # Collapse strided so that it has one more dimension than the window.  I.e.,
+    # the new array is a flat list of slices.
+    meat = len(ws) if ws.shape else 0
+    firstdim = (np.product(newshape[:-meat]),) if ws.shape else ()
+    dim = firstdim + (newshape[-meat:])
+    # remove any dimensions with size 1
+    dim = filter(lambda i : i != 1,dim)
+    
+    return a.reshape(dim), newshape
+    
+def CreateRaster(xx,yy,std,gt,proj,driverName,outFile):  
+    '''
+    Exports data to GTiff Raster
+    '''
+    std = np.squeeze(std)
+    std[np.isinf(std)] = -99
+    std[std>100] = -99
+    driver = gdal.GetDriverByName(driverName)
+    cols = int((np.max(xx) - np.min(xx)) / gt[1])+1
+    rows = int((np.max(yy) - np.min(yy)) / -gt[5])+1
+    ds = driver.Create( outFile, cols, rows, 1, gdal.GDT_Float32)      
+    if proj is not None:  
+        ds.SetProjection(proj.ExportToWkt()) 
+    ds.SetGeoTransform(gt)
+    ss_band = ds.GetRasterBand(1)
+    ss_band.WriteArray(std)
+    ss_band.SetNoDataValue(-99)
+    ss_band.FlushCache()
+    ss_band.ComputeStatistics(False)
+    del ds
+    
+    
+#Stuff to change
+win_sizes = [8,12,20,40]
+for win_size in win_sizes:
+    in_raster = r"C:\workspace\Merged_SS\window_analysis\raster\ss_10_rasterclipped.tif"
+    win = win_size
+    meter = str(win/4)
+    outFile = r"C:\workspace\GLCM\output\2014_04" + os.sep + meter +os.sep+"R01346_R01347_" + meter + ".tif"
+    
+    
+    merge, xx, yy, gt = read_raster(in_raster)
+    
+    merge[np.isnan(merge)] = 0
+    
+    Z,ind = sliding_window(merge,(win,win),(win,win))
+    
+    Ny, Nx = np.shape(merge)
+    
+    w = Parallel(n_jobs = 1, verbose=0)(delayed(p_me)(Z[k]) for k in xrange(len(Z)))
+    
+    std_array = np.reshape(w , ( ind[0], ind[1] ) )
+    std = im_resize(std_array,Nx,Ny)
+    std[merge==0]=np.nan
+    
+    del w,Z,ind,Ny,Nx, std_array
+    
+    driverName= 'GTiff'    
+    epsg_code=26949
+    proj = osr.SpatialReference()
+    proj.ImportFromEPSG(epsg_code)
+    
+    CreateRaster(xx, yy, std, gt, proj,driverName,outFile) 
+    
+    del std, merge, xx, yy, gt, meter
