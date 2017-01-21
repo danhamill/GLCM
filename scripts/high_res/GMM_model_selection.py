55057558852dd8989064c6fe85cb2f35c8303b49
danhamill <hamill.daniel@gmail.com>
2017-01-21 14:49:27 -0700

    inital commit

---
+++ scripts/high_res/GMM_model_selection.py
@@ -0,0 +1,460 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 21 12:23:06 2017
+
+@author: dan
+"""
+
+import numpy as np
+import itertools
+
+from scipy import linalg
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from sklearn import cross_validation
+from sklearn import mixture
+from sklearn import preprocessing
+
+
+def remove_outliers(X, y, k):
+   """
+   simple outlier removal based on deviation from mean
+   """
+   mu, sigma = np.mean(X, axis=0), np.std(X, axis=0, ddof=1)
+   index = np.all(np.abs((X - mu) / sigma) < k, axis=1)
+   return X[index], y[index]
+
+#===========================================================================
+data = np.genfromtxt(r"C:\workspace\GLCM\new_output\merged_aggregraded_distributions.csv", delimiter=',', skip_header=1)
+
+data = data[~np.isnan(data).any(axis=1)]
+
+# for removing outliers
+factor=3 #smaller the number, the more ruthless the pruning
+
+data, sedclass = remove_outliers(data[:,:3], data[:,3], factor)
+data[:,2] = 1 - data[:,2]
+predictors = ['Entropy','Variance','Homogeneity']
+
+#sedclass = data[:,3]
+
+classes = ['sand', 'gravel', 'boulder']
+###########################################################################################################################
+#############################################Variance and Homogeneity
+############################################################################################################################
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:,(1,2)], sedclass, test_size=0.5, random_state=0)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(X_train)
+        bic.append(gmm.bic(X_train))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+spl = plt.subplot(2, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model: Variance + Homogeneity')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+
+# Plot the winner
+splot = plt.subplot(2, 1, 2)
+Y_ = clf.predict(X_train)
+for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,
+                                           color_iter)):
+    print cov
+    v, w = linalg.eigh(cov)
+    if not np.any(Y_ == i):
+        continue
+    plt.scatter(X_train[Y_ == i, 0], X_train[Y_ == i, 1], .8, color=color)
+
+    # Plot an ellipse to show the Gaussian component
+    angle = np.arctan2(w[0][1], w[0][0])
+    angle = 180. * angle / np.pi  # convert to degrees
+    v = 2. * np.sqrt(2.) * np.sqrt(v)
+    ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+    ell.set_clip_box(splot.bbox)
+    ell.set_alpha(.5)
+    splot.add_artist(ell)
+
+plt.xticks(())
+plt.yticks(())
+plt.title('Selected GMM: full model, 3 components')
+plt.subplots_adjust(hspace=.35, bottom=.02)
+plt.show()
+plt.savefig(r"C:\workspace\GLCM\slic_output\model_selection\var_homo.png",dpi=600)
+plt.close()
+
+###########################################################################################################################
+#############################################Entropy and Homogeneity
+############################################################################################################################
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:,(0,2)], sedclass, test_size=0.5, random_state=0)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(X_train)
+        bic.append(gmm.bic(X_train))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+spl = plt.subplot(2, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model: Entropy + Homogeneity')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+
+# Plot the winner
+splot = plt.subplot(2, 1, 2)
+Y_ = clf.predict(X_train)
+for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,
+                                           color_iter)):
+    print cov
+    v, w = linalg.eigh(cov)
+    if not np.any(Y_ == i):
+        continue
+    plt.scatter(X_train[Y_ == i, 0], X_train[Y_ == i, 1], .8, color=color)
+
+    # Plot an ellipse to show the Gaussian component
+    angle = np.arctan2(w[0][1], w[0][0])
+    angle = 180. * angle / np.pi  # convert to degrees
+    v = 2. * np.sqrt(2.) * np.sqrt(v)
+    ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+    ell.set_clip_box(splot.bbox)
+    ell.set_alpha(.5)
+    splot.add_artist(ell)
+
+plt.xticks(())
+plt.yticks(())
+plt.title('Selected GMM: full model, 3 components')
+plt.subplots_adjust(hspace=.35, bottom=.02)
+plt.show()
+plt.savefig(r"C:\workspace\GLCM\slic_output\model_selection\Ent_homo.png",dpi=600)
+plt.close()
+###########################################################################################################################
+#############################################Entropy and Variance
+############################################################################################################################
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:,(0,1)], sedclass, test_size=0.5, random_state=0)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(X_train)
+        bic.append(gmm.bic(X_train))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+spl = plt.subplot(2, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model: Entropy + Variance')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+
+# Plot the winner
+splot = plt.subplot(2, 1, 2)
+Y_ = clf.predict(X_train)
+for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,
+                                           color_iter)):
+    print cov
+    v, w = linalg.eigh(cov)
+    if not np.any(Y_ == i):
+        continue
+    plt.scatter(X_train[Y_ == i, 0], X_train[Y_ == i, 1], .8, color=color)
+
+    # Plot an ellipse to show the Gaussian component
+    angle = np.arctan2(w[0][1], w[0][0])
+    angle = 180. * angle / np.pi  # convert to degrees
+    v = 2. * np.sqrt(2.) * np.sqrt(v)
+    ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+    ell.set_clip_box(splot.bbox)
+    ell.set_alpha(.5)
+    splot.add_artist(ell)
+
+plt.xticks(())
+plt.yticks(())
+plt.title('Selected GMM: full model, 5 components')
+plt.subplots_adjust(hspace=.35, bottom=.02)
+plt.show()
+plt.savefig(r"C:\workspace\GLCM\slic_output\model_selection\Ent_homo.png",dpi=600)
+plt.close()
+###########################################################################################################################
+#############################################Entropy and Variance and Homogeneity
+############################################################################################################################
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:,(0,1,2)], sedclass, test_size=0.5, random_state=0)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(X_train)
+        bic.append(gmm.bic(X_train))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+spl = plt.subplot(2, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model:Entropy +  Variance + Homogeneity')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+
+# Plot the winner
+splot = plt.subplot(2, 1, 2)
+Y_ = clf.predict(X_train)
+for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,
+                                           color_iter)):
+    print cov
+    v, w = linalg.eigh(cov)
+    if not np.any(Y_ == i):
+        continue
+    plt.scatter(X_train[Y_ == i, 0], X_train[Y_ == i, 1], .8, color=color)
+
+    # Plot an ellipse to show the Gaussian component
+    angle = np.arctan2(w[0][1], w[0][0])
+    angle = 180. * angle / np.pi  # convert to degrees
+    v = 2. * np.sqrt(2.) * np.sqrt(v)
+    ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+    ell.set_clip_box(splot.bbox)
+    ell.set_alpha(.5)
+    splot.add_artist(ell)
+
+plt.xticks(())
+plt.yticks(())
+plt.title('Selected GMM: full model, 5 components')
+plt.subplots_adjust(hspace=.35, bottom=.02)
+plt.show()
+plt.savefig(r"C:\workspace\GLCM\slic_output\model_selection\Ent_homo_var.png",dpi=600)
+
+###########################################################################################################################
+#############################################Variance
+############################################################################################################################
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:,1], sedclass, test_size=0.5, random_state=0)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(np.expand_dims(X_train, axis=1) )
+        bic.append(gmm.bic(np.expand_dims(X_train, axis=1) ))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+spl = plt.subplot(1, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model: Variance')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+plt.tight_layout()
+plt.show()
+plt.savefig(r"C:\workspace\GLCM\slic_output\model_selection\Variance.png",dpi=600)
+plt.close()
+###########################################################################################################################
+#############################################Homogeneity
+############################################################################################################################
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:,2], sedclass, test_size=0.5, random_state=0)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(np.expand_dims(X_train, axis=1) )
+        bic.append(gmm.bic(np.expand_dims(X_train, axis=1) ))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+spl = plt.subplot(1, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model: Homogeneity')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+plt.tight_layout()
+plt.show()
+plt.savefig(r"C:\workspace\GLCM\slic_output\model_selection\Homogeneity.png",dpi=600)
+plt.close()
+###########################################################################################################################
+#############################################Entropy
+############################################################################################################################
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:,0], sedclass, test_size=0.5, random_state=0)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(np.expand_dims(X_train, axis=1) )
+        bic.append(gmm.bic(np.expand_dims(X_train, axis=1) ))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+spl = plt.subplot(1, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model: Entropy')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+plt.tight_layout()
+plt.show()
+plt.savefig(r"C:\workspace\GLCM\slic_output\model_selection\Entropy.png",dpi=600)
+plt.close()
